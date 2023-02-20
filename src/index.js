a0a795f7587b832a0bc45db2d641695a13c53be9
Marko Bachvarovski <marko.bachvarovski@mail.utoronto.ca>
2023-02-19 19:18:47 -0500

    Began some work on mouse shadow

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import initTiltEffect from "./scripts/tiltAnimation";
 import createCloud from "./data/tagCloud";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 import seeMoreButtonListener from "./scripts/seeMore";
+import mouseShadowHandler from "./scripts/mouseShadow";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
 seeMoreButtonListener();
+// mouseShadowHandler();
+// alert("Hi")
 
 if (screen.width > 750) {
   createCloud(350);
---
+++ src/scripts/mouseShadow.js
@@ -0,0 +1,14 @@
+// const shadow = document.getElementById("mouseShadow");
+
+export default function mouseShadowHandler() {
+  const shadow = document.getElementById("mouseShadow");
+  console.log("out");
+
+  // document.body.onpointermove = (event) => {
+  //   console.log("Inside");
+  //   const { xpos, ypos } = event;
+  //
+  //   shadow.style.left = `${xpos}px`;
+  //   shadow.style.top = `${ypos}px`;
+  // };
+}
