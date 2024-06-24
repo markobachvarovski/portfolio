02ec210443aee98d6bae3a1c702b8edce7fc4051
Marko Bachvarovski <marko.bachvarovski@gmail.com>
2024-06-23 22:53:33 -0400

    Adding ShakespeareGPT row

--- src/scripts/hiddenRows.js
+++ src/scripts/hiddenRows.js
@@ -1,94 +1,94 @@
 const computationalPhysicsRow =
-  '            <div class="col-lg-4 col-sm-12">\n' +
-  '              <div class="project-wrapper__text project-wrapper__text__hidden">\n' +
-  '                <h3 class="project-wrapper__text-title">Computational Physics</h3>\n' +
-  "                <div>\n" +
-  '                  <p class="mb-4">\n' +
-  "                    This is coursework from the PHY407 course at the University of Toronto.\n" +
-  "                    We've implemented several algorithms and techniques to computationally\n" +
-  "                    solve different physics problems, including Monte Carlo simulation,\n" +
-  "                    Markov Chains, ODE and PDE solvers, Fourier decomposition algorithm, \n" +
-  "                    Metropolis algorithm, LU decomposition and backsubstitution algorithms for\n" +
-  "                    matrix decomposition and more!\n" +
-  "                    Note: Repository has been made private to maintain academic integrity\n" +
-  "                  </p>\n" +
-  "                </div>\n" +
-  "                <a\n" +
-  '                  rel="noreferrer"\n' +
-  '                  target="_blank"\n' +
-  '                  class="cta-btn cta-btn--hero"\n' +
-  '                  href="https://github.com/markobachvarovski/PHY407"\n' +
-  "                >\n" +
-  "                  Source code\n" +
-  "                </a>\n" +
-  "              </div>\n" +
-  "            </div>\n" +
-  '            <div class="col-lg-8 col-sm-12">\n' +
-  '              <div class="project-wrapper__image project-wrapper__image__hidden">\n' +
-  '                <a rel="noreferrer" href="#!" target="_blank">\n' +
-  "                  <div\n" +
-  '                    id="img-robofriends"' +
-  "                    data-tilt\n" +
-  '                    data-tilt-max="4"\n' +
-  '                    data-tilt-glare="true"\n' +
-  '                    data-tilt-max-glare="0.5"\n' +
-  '                    class="thumbnail rounded js-tilt"\n' +
-  "                  >\n" +
-  '                    <img alt="Computational Physics Image" class="img-fluid" ' +
-  '                         src="https://www.physicsread.com/wp-content/uploads/2021/06/computational-physics.png"/> ' +
-  "                  </div>\n" +
-  "                </a>\n" +
-  "              </div>\n" +
-  "            </div>\n";
+    '            <div class="col-lg-4 col-sm-12">\n' +
+    '              <div class="project-wrapper__text project-wrapper__text__hidden">\n' +
+    '                <h3 class="project-wrapper__text-title">Computational Physics</h3>\n' +
+    "                <div>\n" +
+    '                  <p class="mb-4">\n' +
+    "                    This is coursework from the PHY407 course at the University of Toronto.\n" +
+    "                    We've implemented several algorithms and techniques to computationally\n" +
+    "                    solve different physics problems, including Monte Carlo simulation,\n" +
+    "                    Markov Chains, ODE and PDE solvers, Fourier decomposition algorithm, \n" +
+    "                    Metropolis algorithm, LU decomposition and backsubstitution algorithms for\n" +
+    "                    matrix decomposition and more!\n" +
+    "                    Note: Repository has been made private to maintain academic integrity\n" +
+    "                  </p>\n" +
+    "                </div>\n" +
+    "                <a\n" +
+    '                  rel="noreferrer"\n' +
+    '                  target="_blank"\n' +
+    '                  class="cta-btn cta-btn--hero"\n' +
+    '                  href="https://github.com/markobachvarovski/PHY407"\n' +
+    "                >\n" +
+    "                  Source code\n" +
+    "                </a>\n" +
+    "              </div>\n" +
+    "            </div>\n" +
+    '            <div class="col-lg-8 col-sm-12">\n' +
+    '              <div class="project-wrapper__image project-wrapper__image__hidden">\n' +
+    '                <a rel="noreferrer" href="#!" target="_blank">\n' +
+    "                  <div\n" +
+    '                    id="img-robofriends"' +
+    "                    data-tilt\n" +
+    '                    data-tilt-max="4"\n' +
+    '                    data-tilt-glare="true"\n' +
+    '                    data-tilt-max-glare="0.5"\n' +
+    '                    class="thumbnail rounded js-tilt"\n' +
+    "                  >\n" +
+    '                    <img alt="Computational Physics Image" class="img-fluid" ' +
+    '                         src="https://www.physicsread.com/wp-content/uploads/2021/06/computational-physics.png"/> ' +
+    "                  </div>\n" +
+    "                </a>\n" +
+    "              </div>\n" +
+    "            </div>\n";
 
 const robofriendsRow =
-  '            <div class="col-lg-4 col-sm-12">\n' +
-  '              <div class="project-wrapper__text project-wrapper__text__hidden">\n' +
-  '                <h3 class="project-wrapper__text-title">Robofriends</h3>\n' +
-  "                <div>\n" +
-  '                  <p class="mb-4">\n' +
-  "                    This project was my first attempt at using React and connecting\n" +
-  "                    a project to an API. The users are fetched from a JSON\n" +
-  "                    placeholder website online\n" +
-  "                    and passed through an API that assigns each user to a random\n" +
-  "                    image of a robot based on their name. The user is able to\n" +
-  "                    search and scroll through the robots to find a specific one.\n" +
-  "                  </p>\n" +
-  "                </div>\n" +
-  "                <a\n" +
-  '                  rel="noreferrer"\n' +
-  '                  target="_blank"\n' +
-  '                  class="cta-btn cta-btn--hero"\n' +
-  '                  href="https://markobachvarovski.github.io/robofriends/"\n' +
-  "                >\n" +
-  "                  See Live\n" +
-  "                </a>\n" +
-  "                <a\n" +
-  '                  rel="noreferrer"\n' +
-  '                  target="_blank"\n' +
-  '                  class="cta-btn text-color-main"\n' +
-  '                  href="https://github.com/markobachvarovski/robofriends"\n' +
-  "                >\n" +
-  "                  Source Code\n" +
-  "                </a>\n" +
-  "              </div>\n" +
-  "            </div>\n" +
-  '            <div class="col-lg-8 col-sm-12">\n' +
-  '              <div class="project-wrapper__image project-wrapper__image__hidden">\n' +
-  '                <a rel="noreferrer" href="#!" target="_blank">\n' +
-  "                  <div\n" +
-  '                    id="img-robofriends"' +
-  "                    data-tilt\n" +
-  '                    data-tilt-max="4"\n' +
-  '                    data-tilt-glare="true"\n' +
-  '                    data-tilt-max-glare="0.5"\n' +
-  '                    class="thumbnail rounded js-tilt"\n' +
-  "                  >\n" +
-  '                    <img alt="Robofriends Image" class="img-fluid" src="https://i.imgur.com/4V2KQw8.png"/> ' +
-  "                  </div>\n" +
-  "                </a>\n" +
-  "              </div>\n" +
-  "            </div>\n";
+    '            <div class="col-lg-4 col-sm-12">\n' +
+    '              <div class="project-wrapper__text project-wrapper__text__hidden">\n' +
+    '                <h3 class="project-wrapper__text-title">Robofriends</h3>\n' +
+    "                <div>\n" +
+    '                  <p class="mb-4">\n' +
+    "                    This project was my first attempt at using React and connecting\n" +
+    "                    a project to an API. The users are fetched from a JSON\n" +
+    "                    placeholder website online\n" +
+    "                    and passed through an API that assigns each user to a random\n" +
+    "                    image of a robot based on their name. The user is able to\n" +
+    "                    search and scroll through the robots to find a specific one.\n" +
+    "                  </p>\n" +
+    "                </div>\n" +
+    "                <a\n" +
+    '                  rel="noreferrer"\n' +
+    '                  target="_blank"\n' +
+    '                  class="cta-btn cta-btn--hero"\n' +
+    '                  href="https://markobachvarovski.github.io/robofriends/"\n' +
+    "                >\n" +
+    "                  See Live\n" +
+    "                </a>\n" +
+    "                <a\n" +
+    '                  rel="noreferrer"\n' +
+    '                  target="_blank"\n' +
+    '                  class="cta-btn text-color-main"\n' +
+    '                  href="https://github.com/markobachvarovski/robofriends"\n' +
+    "                >\n" +
+    "                  Source Code\n" +
+    "                </a>\n" +
+    "              </div>\n" +
+    "            </div>\n" +
+    '            <div class="col-lg-8 col-sm-12">\n' +
+    '              <div class="project-wrapper__image project-wrapper__image__hidden">\n' +
+    '                <a rel="noreferrer" href="#!" target="_blank">\n' +
+    "                  <div\n" +
+    '                    id="img-robofriends"' +
+    "                    data-tilt\n" +
+    '                    data-tilt-max="4"\n' +
+    '                    data-tilt-glare="true"\n' +
+    '                    data-tilt-max-glare="0.5"\n' +
+    '                    class="thumbnail rounded js-tilt"\n' +
+    "                  >\n" +
+    '                    <img alt="Robofriends Image" class="img-fluid" src="https://i.imgur.com/4V2KQw8.png"/> ' +
+    "                  </div>\n" +
+    "                </a>\n" +
+    "              </div>\n" +
+    "            </div>\n";
 
 const passwordCheckerRow =
     '            <div class="col-lg-4 col-sm-12">\n' +
@@ -137,4 +137,59 @@ const passwordCheckerRow =
     "              </div>\n" +
     "            </div>\n";
 
-export default [passwordCheckerRow, computationalPhysicsRow, robofriendsRow];
+const smartBrainRow =
+    '            <div class="col-lg-4 col-sm-12">\n' +
+    '              <div class="project-wrapper__text project-wrapper__text__hidden">\n' +
+    '                <h3 class="project-wrapper__text-title">Smart Brain</h3>\n' +
+    "                <div>\n" +
+    '                  <p class="mb-4">\n' +
+    "                    This is a project made with React and connected to a facial\n" +
+    '                    recognition API. The user must register and upon\n' +
+    '                    registration, they are able to enter a URL of an image,\n' +
+    '                    which gets displayed on the screen. If the image contains\n' +
+    '                    faces, the app will recognize them and put a box around\n' +
+    '                    them. The front end is connected to a server and a database,\n' +
+    '                    so that a user can access it at any time. <br/>\n' +
+    '                    Note: API is currently down, users might experience bugs\n' +
+    "                  </p>\n" +
+    "                </div>\n" +
+    '                <a\n' +
+    '                    rel="noreferrer"\n' +
+    '                    target="_blank"\n' +
+    '                    class="cta-btn cta-btn--hero"\n' +
+    '                    href="https://marko-smart-brain.herokuapp.com/"\n' +
+    '                >\n' +
+    '                   See Live\n' +
+    '                </a>\n' +
+    "                <a\n" +
+    '                  rel="noreferrer"\n' +
+    '                  target="_blank"\n' +
+    '                  class="cta-btn text-color-main"\n' +
+    '                  href="https://github.com/markobachvarovski/final-project"\n' +
+    "                >\n" +
+    "                  Source Code\n" +
+    "                </a>\n" +
+    "              </div>\n" +
+    "            </div>\n" +
+    '            <div class="col-lg-8 col-sm-12">\n' +
+    '              <div class="project-wrapper__image project-wrapper__image__hidden">\n' +
+    '                <a rel="noreferrer" href="https://marko-smart-brain.herokuapp.com" target="_blank">\n' +
+    "                  <div\n" +
+    '                    id="img-password-checker"' +
+    "                    data-tilt\n" +
+    '                    data-tilt-max="4"\n' +
+    '                    data-tilt-glare="true"\n' +
+    '                    data-tilt-max-glare="0.5"\n' +
+    '                    class="thumbnail rounded js-tilt"\n' +
+    "                  >\n" +
+    '                    <img\n' +
+    '                      alt="Smart Brain"\n' +
+    '                      class="img-fluid"\n' +
+    '                      src="https://i.imgur.com/y7C98Db.png"\n' +
+    '                    /> ' +
+    "                  </div>\n" +
+    "                </a>\n" +
+    "              </div>\n" +
+    "            </div>\n";
+
+export default [smartBrainRow, passwordCheckerRow, computationalPhysicsRow, robofriendsRow];
